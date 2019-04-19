92fb2033d128016d079be0bebdc42004b6d49db6
Babausse <courtois.vincent@outlook.com>
2019-04-19 11:03:06 +0200

    Overcome the items limit for links by embedding it

--- generator.rb
+++ generator.rb
@@ -3,17 +3,15 @@ require 'faker'
 require './parts/cities.rb'
 require './parts/champions.rb'
 require'./parts/players'
-require'./parts/links'
 
 champions = generate_champions
 cities = generate_cities
-players = generate_players(cities)
+players = generate_players(cities, champions)
 
 structure = {
   cities: cities,
   champions: champions,
-  players: players,
-  links: generate_links(champions, players)
+  players: players
 }
 
 puts structure.to_json
--- parts/players.rb
+++ parts/players.rb
@@ -1,9 +1,15 @@
-def generate_players(cities)
+def generate_players(cities, champions)
   return Array.new(30).map.with_index do |player, index|
+    Faker::Number.unique.clear
     {
       playerId: index + 1,
       name: Faker::TvShows::BreakingBad.unique.character,
-      city: cities.sample
+      city: cities.sample,
+      champions: Array.new(3).map do
+        {
+          championId: Faker::Number.unique.between(1, 30)
+        }
+      end
     }
   end
 end
